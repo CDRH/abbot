d35c0fb0d9b17ed843905aada6333d6cc7935bcd
Stephen Ramsay <sramsay.unl@gmail.com>
2012-01-12 13:08:00 -0600

    Basic API is working.
    Refactored core.clj into cli.clj and Abbot.clj.

---
+++ src/edu/unl/abbot/cli.clj
@@ -0,0 +1,37 @@
+
+;;; cli.clj
+;;;
+;;; This file is part of Abbot.  It contains the entry point for
+;;; the command-line interface.
+;;;
+;;; Written and maintained by Brian Pytlik-Zillig and Stephen Ramsay
+;;; for the Center for Digital Research in the Humanities at the
+;;; University of Nebraska-Lincoln.
+;;;
+;;; Last Modified: Sun Jan 01 09:14:10 CST 2012
+;;;
+;;; Copyright © 2011 Board of Regents of the University of Nebraska-
+;;; Lincoln (and others).  See LICENSE for details.
+;;;
+;;; Abbot is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See LICENSE
+;;; for more details.
+
+(ns edu.unl.abbot.cli
+	(:use edu.unl.abbot.core)
+  (:use edu.unl.abbot.stylesheets)
+  (:use edu.unl.abbot.utils)
+  (import
+    (java.io File)) 
+		(:gen-class))
+
+(require '[clojure.tools.cli :as c])
+
+(defn -main [& args]
+  "Process command-line switches and call main conversion function"
+  (let [opts (c/cli args
+    (c/optional ["-s" "--single" "Run in single-threaded mode" :default false]) 
+    (c/optional ["-i" "--inputdir" "Input directory path" :default (str abbot-home "/input")])
+    (c/optional ["-o" "--outputdir" "Output directory path" :default (str abbot-home "/output/")]))]
+    (convert-files opts)))
