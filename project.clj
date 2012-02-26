107d1008cee8c27a818bd088f9e830d1145593d9
Stephen Ramsay <sramsay.unl@gmail.com>
2012-02-26 16:18:01 -0600

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject edu.unl/abbot "0.3.3"
+(defproject edu.unl/abbot "0.3.4"
   :description "Abbot: A Conversion Tool for Text Interoperability"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.1.0"]
