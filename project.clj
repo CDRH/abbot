6de0f6abba7f09fb4e9dd350091e6a6a0fb034ce
Stephen Ramsay <sramsay.unl@gmail.com>
2012-03-04 16:45:17 -0600

    Updated clojure.tools.cli.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject edu.unl/abbot "0.4.1"
+(defproject edu.unl/abbot "0.4.2"
   :description "Abbot: A Conversion Tool for Text Interoperability"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.cli "0.1.0"]
+                 [org.clojure/tools.cli "0.2.1"]
                  ;[org.clojars.ninjudd/data.xml "0.0.1-SNAPSHOT"]
                  [clojure-saxon "0.9.2"]]
 	:keep-non-project-classes true
