26736b557c13dc70d0946d1988b90d0fc89eec4d
Stephen Ramsay <sramsay.unl@gmail.com>
2012-07-31 16:10:08 -0500

    Added data.xml dependency.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Abbot: A Conversion Tool for Text Interoperability"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
-                 ;[org.clojars.ninjudd/data.xml "0.0.1-SNAPSHOT"]
+                 [org.clojure/data.xml "0.0.6"]
                  [clojure-saxon "0.9.2"]]
 	:keep-non-project-classes true
 	:main edu.unl.abbot.cli
