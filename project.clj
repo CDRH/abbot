a373d337bb56a52d3093d48c903a982806e83b30
Stephen Ramsay <sramsay.unl@gmail.com>
2013-01-02 16:12:52 -0600

    Upgrading clojure to 1.5.0-RC1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject edu.unl/abbot "0.5.0"
   :description "Abbot: A Conversion Tool for Text Interoperability"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/data.xml "0.0.6"]
                  [clojure-saxon "0.9.3"]]
