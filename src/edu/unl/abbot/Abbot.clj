13444acfa5b4db79a661095435f4111ddedd6fff
Stephen Ramsay <sramsay.unl@gmail.com>
2013-01-11 19:48:25 -0600

    Reformat.

--- src/edu/unl/abbot/Abbot.clj
+++ src/edu/unl/abbot/Abbot.clj
@@ -7,7 +7,7 @@
 ;;; for the Center for Digital Research in the Humanities at the
 ;;; University of Nebraska-Lincoln.
 ;;;
-;;; Last Modified: Fri Jan 11 19:32:06 CST 2013
+;;; Last Modified: Fri Jan 11 19:48:21 CST 2013
 ;;;
 ;;; Copyright © 2011-2013 Board of Regents of the University of Nebraska-
 ;;; Lincoln (and others).  See COPYING for details.
@@ -18,19 +18,18 @@
 ;;; for more details.
 
 (ns edu.unl.abbot.Abbot
-	(:use edu.unl.abbot.core)
+    (:use edu.unl.abbot.core)
   (:use edu.unl.abbot.stylesheets)
   (:use edu.unl.abbot.utils)
   (import
     (java.io File))
-	(:gen-class
-  	:name edu.unl.abbot.Abbot
-  	:init init
-  	:state state
-  	:methods [
-			[convert [String String String] void]
-			[convert [String String String String] void]
-			[convert [String String String String String] void]]))
+    (:gen-class
+    :name edu.unl.abbot.Abbot
+    :init init
+    :state state
+    :methods [[convert [String String String] void]
+						  [convert [String String String String] void]
+              [convert [String String String String String] void]]))
 
 (defn -init []
 	[[] (atom [])])
@@ -39,22 +38,22 @@
 	"Apply the conversion stylesheet to the input files."
 	([this inputdir outputdir target]
 		(let [opts {:inputdir inputdir
-								:outputdir outputdir
-								:target target
-								:namespace "http://www.tei-c.org/ns/1.0"
-								:custom "http://abbot.unl.edu/abbot_config.xml"}]
+                :outputdir outputdir
+                :target target
+                :namespace "http://www.tei-c.org/ns/1.0"
+                :custom "http://abbot.unl.edu/abbot_config.xml"}]
 			(convert-files opts)))
 	([this inputdir outputdir target namespace]
-	(let [opts {:inputdir inputdir
-			  			:outputdir outputdir
-							:target target
-							:namespace namespace
-							:custom "http://abbot.unl.edu/abbot_config.xml"}]
+    (let [opts {:inputdir inputdir
+                :outputdir outputdir
+                :target target
+                :namespace namespace
+                :custom "http://abbot.unl.edu/abbot_config.xml"}]
 			(convert-files opts)))
 	([this inputdir outputdir target namespace custom]
-	(let [opts {:inputdir inputdir
-			  			:outputdir outputdir
-							:target target
-							:namespace namespace
-							:custom custom}]
+    (let [opts {:inputdir inputdir
+                :outputdir outputdir
+                :target target
+                :namespace namespace
+                :custom custom}]
 			(convert-files opts))))
