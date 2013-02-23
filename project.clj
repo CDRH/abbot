f20a17cd425e36136bd84f5993044ef9967745e0
Stephen Ramsay <sramsay.unl@gmail.com>
2013-02-22 20:28:26 -0600

    RC17

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject edu.unl/abbot "0.7.1"
   :description "Abbot: A Conversion Tool for Text Interoperability"
-  :dependencies [[org.clojure/clojure "1.5.0-RC16"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC17"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/data.xml "0.0.7"]
                  [clojure-saxon "0.9.3"]]
