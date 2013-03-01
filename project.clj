19ee2f35686c650b95fb0141f956bc2f94812515
Stephen Ramsay <sramsay.unl@gmail.com>
2013-03-01 12:35:21 -0600

    Clojure 1.5.0 stable; version bump;

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject edu.unl/abbot "0.7.1"
+(defproject edu.unl/abbot "0.7.2"
   :description "Abbot: A Conversion Tool for Text Interoperability"
-  :dependencies [[org.clojure/clojure "1.5.0-RC17"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/data.xml "0.0.7"]
                  [clojure-saxon "0.9.3"]]
--- src/edu/unl/abbot/utils.clj
+++ src/edu/unl/abbot/utils.clj
@@ -22,7 +22,7 @@
 	(:import
 		(java.io File)))
 
-(def version "0.7.1")
+(def version "0.7.2")
 
 (def abbot-home (System/getenv "ABBOT_HOME"))
 
